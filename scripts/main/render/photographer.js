9fac86be3997393ce1b52a56e270bed5ceeb38ff
shadow <oleeefr@yahoo.fr>
2022-04-19 11:40:41 +0200

    ajout cosmetique de la page

--- scripts/main/render/photographer.js
+++ scripts/main/render/photographer.js
@@ -2,7 +2,7 @@ import { routes } from "../../config/routes.js";
 
 export function photographerFactory(data) {
     const __PATH ='../';
-    const { name, portrait } = data;
+    const { name, portrait, country, city, tagline, price} = data;
     const no_image = __PATH+"assets/photographers/account.png" ;
     const picture = (portrait ==null || 
                      portrait.trim().length == 0)? no_image: routes.__DIRNAME_PHOTOGRAPHER_PORTRAIT()+portrait;
@@ -11,10 +11,22 @@ export function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+        const dl = document.createElement( 'dl');
+        const dt = document.createElement( 'dt');
+        dt.textContent = name;
+        const ddLocalisation = document.createElement( 'dd');
+        ddLocalisation.textContent = city+", "+country;
+        const ddTagline = document.createElement( 'dd');
+        ddTagline.textContent = tagline;
+        const ddPrice = document.createElement( 'dd');
+        ddPrice.setAttribute("class", "price");
+        ddPrice.textContent = price+"€/jour";
+        dl.appendChild(dt);
+        dl.appendChild(ddLocalisation);
+        dl.appendChild(ddTagline);
+        dl.appendChild(ddPrice);
         article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(dl);
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/main/requests/selectAllMediaByIdPhotographer.js
+++ scripts/main/requests/selectAllMediaByIdPhotographer.js
@@ -1,1 +1,5 @@
-import {} from '../../lib/requests/listMediaByIdPhotographer.js'; 
+import {listPhotographers, listMedia} from '../../lib/requests/listMediaByIdPhotographer.js'; 
+
+import {} from '../../lib/requests/photographerById.js';
+
+import {} from '../../lib/requests/listMediaByIdPhotographer.js';
