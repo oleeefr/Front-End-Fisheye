71938a24e5a7cbac40f73c8d1bef5a6f3c59dabf
shadow <oleeefr@yahoo.fr>
2022-04-26 09:39:54 +0200

    insertion du concept controller

---
+++ scripts/main/controller/photographer.js
@@ -0,0 +1,4 @@
+import {photograph} from '../requests/getAllMediaByIdPhotographer.js';
+import {photographerFactory} from '../../lib/render/photographer.js';
+
+export const moduleHtmlPhotographer = photographerFactory(pageModuleData.photograph);
